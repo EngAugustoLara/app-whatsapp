5a793ba4f216217f01b79ff7208c14f0775ecc0e
Wesley Ramos <wesley-ramos@outlook.com.br>
2017-06-07 04:19:39 -0300

    Permissões e alteração no Layout

--- app/src/main/java/app/ctrlsoftware/com/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/app/ctrlsoftware/com/whatsapp/activity/LoginActivity.java
@@ -1,14 +1,15 @@
-package app.ctrlsoftware.com.whatsapp;
+package app.ctrlsoftware.com.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    private String test;
+import app.ctrlsoftware.com.whatsapp.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
