7b3da7478330587df276b55cfe00aa861f35b652
Wesley Ramos <wesley-ramos@outlook.com.br>
2017-06-07 07:06:11 -0300

    Gerando "Token"

--- app/src/main/java/app/ctrlsoftware/com/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/app/ctrlsoftware/com/whatsapp/activity/LoginActivity.java
@@ -2,12 +2,16 @@ package app.ctrlsoftware.com.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.github.rtoshiro.util.format.MaskFormatter;
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 import com.github.rtoshiro.util.format.text.SimpleMaskTextWatcher;
+
+import java.util.Random;
 
 import app.ctrlsoftware.com.whatsapp.R;
 
@@ -17,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText fone;
     private EditText foneCodigoDdd;
     private EditText foneCodigoPais;
+    private Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +32,11 @@ public class LoginActivity extends AppCompatActivity {
         foneCodigoPais = (EditText) findViewById(R.id.caixaFoneCodigoPaisId);
         foneCodigoDdd = (EditText) findViewById(R.id.caixaFoneCodigoDddId);
         fone = (EditText) findViewById(R.id.caixaFoneId);
+        btnCadastrar = (Button) findViewById(R.id.btnCadastrarId);
 
 
 
+        //Definindo as máscaras
         SimpleMaskFormatter simpleMaskCodigoPais = new SimpleMaskFormatter("+NN");
         SimpleMaskFormatter simpleMaskCodigoDdd = new SimpleMaskFormatter("NN");
         SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter("NNNNN-NNNN");
@@ -42,5 +49,28 @@ public class LoginActivity extends AppCompatActivity {
         foneCodigoDdd.addTextChangedListener(maskCodigoDdd);
         fone.addTextChangedListener(maskTelefone);
 
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String nomeUsuario = nome.getText().toString();
+                String telefoneCompleto = foneCodigoPais.getText().toString() +
+                        foneCodigoDdd.getText().toString() +
+                        fone.getText().toString();
+                //Retirando as mascaras e deixando o telefone sem formatação
+                String telefoneSemFormatacao = telefoneCompleto.replace("+", "");
+                telefoneSemFormatacao = telefoneCompleto.replace("-", "");
+
+                //Gerar Token
+
+                Random randomico = new Random();
+                int numeroRandomico = randomico.nextInt(9999 - 1000) + 1000;
+
+                String token = String.valueOf(numeroRandomico);
+
+
+
+            }
+        });
     }
 }
