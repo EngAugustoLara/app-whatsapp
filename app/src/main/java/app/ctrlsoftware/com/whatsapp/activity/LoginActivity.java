40f01e7073fcf91e3116743d27b7fc8717df069b
Wesley Ramos <wesley-ramos@outlook.com.br>
2017-06-09 03:46:45 -0300

    Update info, cod, icon

--- app/src/main/java/app/ctrlsoftware/com/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/app/ctrlsoftware/com/whatsapp/activity/LoginActivity.java
@@ -1,19 +1,100 @@
 package app.ctrlsoftware.com.whatsapp.activity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 import app.ctrlsoftware.com.whatsapp.R;
+import app.ctrlsoftware.com.whatsapp.config.ConfiguracaoFirebase;
+import app.ctrlsoftware.com.whatsapp.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginEmail;
+    private EditText loginSenha;
+    private Button btnLogar;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        verificarUsuarioLogado();
+
+        loginEmail = (EditText) findViewById(R.id.editLoginEmailId);
+        loginSenha = (EditText) findViewById(R.id.editLoginSenhaId);
+        btnLogar = (Button) findViewById(R.id.btnLogarId);
+
+        btnLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                usuario = new Usuario();
+                usuario.setEmail( loginEmail.getText().toString() );
+                usuario.setSenha( loginSenha.getText().toString() );
+
+                validarLogin();
+            }
+        });
+    }
+
+    private void verificarUsuarioLogado(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        if (autenticacao.getCurrentUser() != null) {
+            abrirTelaPrincipal();
+        }
+    }
+
+    private void validarLogin(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if ( task.isSuccessful() ) {
+                    abrirTelaPrincipal();
+                    Toast.makeText(LoginActivity.this, "Sucesso ao fazer login", Toast.LENGTH_SHORT).show();
+
+                } else{
+                    String erroExecao = "";
+
+                    try{
+                        throw task.getException();
+                    } catch (FirebaseAuthInvalidUserException e) {
+                        erroExecao = "A conta de email não existe ou foi desativada";
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        erroExecao = "Senha incorreta";
+                    } catch (Exception e) {
+                        erroExecao = "Ao efeturar login";
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(LoginActivity.this, "Erro: "+erroExecao, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    private void abrirTelaPrincipal(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity( intent );
+        finish();
     }
 
     public void abrirCadastroUsuario(View view){
--- app/src/main/java/app/ctrlsoftware/com/whatsapp/activity/MainActivity.java
+++ app/src/main/java/app/ctrlsoftware/com/whatsapp/activity/MainActivity.java
@@ -1,8 +1,15 @@
 package app.ctrlsoftware.com.whatsapp.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -11,11 +18,23 @@ import app.ctrlsoftware.com.whatsapp.config.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar( toolbar );
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
